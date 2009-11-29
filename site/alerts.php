596ccb22a9a92e0ba897b5c385be4191886271d1
Thiago <tjcampos@gmail.com>
2009-11-29 18:06:20 -0200

    implementado info.php

--- site/alerts.php
+++ site/alerts.php
@@ -3,7 +3,7 @@ require("config.php");
 $conn = mysql_pconnect($ip,$user,$pass) or die("Falha ao conectar ao banco de dados.");
 mysql_select_db($database,$conn) or die("Falha ao selecionar o banco de dados");
 
-$query = "SELECT name, description.text AS info, TIME(timestamp) AS timestamp
+$query = "SELECT event.id, name, description.text AS info, TIME(timestamp) AS timestamp
 FROM event JOIN client ON id_client = client.id
 LEFT JOIN description ON event.description = description.id
 WHERE status = 0
@@ -21,7 +21,7 @@ $result = mysql_query($query,$conn) or die ("Falha ao consultar o banco");
 while ($line = mysql_fetch_array($result)) {
 	?>
 		<tr>
-		<td><? echo $line['name']; ?></td>
+		<td><a href=info.php?id=<? echo $line['id'] ?>><? echo $line['name']; ?></a></td>
 		<td><? echo $line['info']; ?></td>
 		<td><? echo $line['timestamp']; ?></td>
 		</tr>
---
+++ site/info.php
@@ -0,0 +1,25 @@
+<?
+require("config.php");
+$id = $_GET['id'];
+$conn = mysql_pconnect($ip,$user,$pass) or die("Falha ao conectar ao banco de dados.");
+mysql_select_db($database,$conn) or die("Falha ao selecionar o banco de dados");
+
+$query = "SELECT name, age, obs, text AS info, latitude, longitude, timestamp, status
+	FROM event JOIN client ON id_client=client.id 
+	LEFT JOIN description ON description = description.id 
+	WHERE event.id=$id";
+$result = mysql_query($query,$conn) or die ("Falha ao consultar o banco");
+
+while ($line = mysql_fetch_array($result)) {
+	?>
+		
+		Nome:&nbsp;<? echo $line['name']; ?></br>
+		Idade:&nbsp;<? echo $line['age']; ?></br>
+		Observa&ccedil;&atilde;o:&nbsp;<? echo $line['obs']; ?></br>
+		Motivo:&nbsp;<? echo $line['info']; ?></br>
+		Latitude:&nbsp;<? echo $line['latitude']; ?></br>
+		Longitude:&nbsp;<? echo $line['longitude']; ?></br>
+		Data:&nbsp;<? echo $line['timestamp']; ?>
+	<?
+}
+?>
